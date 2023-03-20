9e07e8c05f8cd258cf027be037b00d921ce2edef
Sergii <sergii.khylko@outlook.com>
2023-03-20 18:39:22 +0200

    #0 init project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NegativeCounter from "./components/NegativeCounter";
+import PositiveCounter from "./components/PositiveCounter";
+
+function App() {
+  return (
+   <div>Hello World</div>
+  );
+}
+
+export default App;
